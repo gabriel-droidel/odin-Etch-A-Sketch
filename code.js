e769e41a16ec02047c52a7af72e43d206a21be30
gabi droid <193822798+gabriel-droidel@users.noreply.github.com>
2025-01-17 09:27:16 +0100

    Add button on header and set limits on size input

--- code.js
+++ code.js
@@ -1,59 +1,60 @@
 //Add event on the html button to avoid global variables pollution
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.querySelector('.start');
-    button.addEventListener('click', ()=>generateGrid(getData()));
+    button.addEventListener('click', ()=>generateGrid(16));
     const gridButton = document.querySelector('.gridSize');
-    gridButton.addEventListener('click', ()=>changeGridSize(data))
+    gridButton.addEventListener('click', ()=>changeGridSize())
 });
 
-function generateGrid(data)
+function generateGrid(size)
 {
     // get css data to adjust grid size accordingly
     const contentBox = document.querySelector('.content');
     contentBox.textContent='';
     const drawingBoard = document.createElement('div');
-    drawingBoard.classList.add('content');
+    drawingBoard.classList.add('content-box');
     contentBox.appendChild(drawingBoard);
 
-        for(let column=0;column<data.boxNumber;column++)
+        for(let column=0;column<size;column++)
         {
-            for(let row=0;row<data.boxNumber;row++)
+            for(let row=0;row<size;row++)
             {
                 const box = document.createElement('div');
                 box.classList='box-style';
+                box.style.height=480/size + `px`;
+                box.style.width=480/size + `px`;
                 drawingBoard.appendChild(box);
             }
         }
         drawingBoard.addEventListener('click', ()=>  drawGrid());
-
-        return;
-}
-
-function getData()
-{
-    // Get data from css custom properties 
-    // Return object with css properties
-    const root = document.documentElement;
-    const styles = getComputedStyle(root);
-    const boxCount = styles.getPropertyValue('--box-per-row');
-    const boxSize = styles.getPropertyValue('--box-size');
-    data = {
-        boxNumber: boxCount,
-        boxSize : boxSize, 
-    };
-    return data;
 }
 
 function drawGrid(){
+    const drawingBoard =document.querySelector('.content-box')
     const createdBoxes = document.querySelectorAll('.box-style');
     createdBoxes.forEach(box =>{
         box.addEventListener('mouseover', ()=>{
             box.classList.add('hovered');
         });
     });
+    drawingBoard.addEventListener('click', ()=>{
+        createdBoxes.forEach(box =>{
+            box.removeEventListener('mouseover', ()=>{});
+        });
+    });
 }
 
-function changeGridSize(data){
-    data=getData();
-    
+function changeGridSize(){
+    let keepGoing=true;
+    let size=0;
+    while(keepGoing)
+    {
+        size = prompt('Choose your grid size (maximum 100, minimum 2)');
+        if(size>100||size<1)
+            alert('Less than 100 please!');
+        else {
+            generateGrid(size);
+            keepGoing=false;
+        }
+    }
 }
